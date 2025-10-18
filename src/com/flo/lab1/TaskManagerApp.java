b287a3f394a767bfc2b17bfe82786728d4866fc4
floreaGabriel <g.florea2004@gmail.com>
2025-10-19 00:33:29 +0300

    [ADD] Added read/write from/to file to save the state of the app.

--- src/com/flo/lab1/TaskManagerApp.java
+++ src/com/flo/lab1/TaskManagerApp.java
@@ -1,5 +1,6 @@
 package com.flo.lab1;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -12,6 +13,7 @@ public class TaskManagerApp {
         System.out.println("\n=== Task Manager ===");
 
         TaskManagerApp app = new TaskManagerApp();
+        app.loadTaskFromFile("tasks.txt");
 
         Scanner sc = new Scanner(System.in);
         boolean running = true;
@@ -31,19 +33,19 @@ public class TaskManagerApp {
             switch (opt) {
                 case 1 -> {
                     System.out.println("[ADD] Specifica nume: ");
-                    String nume = sc.next();
+                    String nume = sc.nextLine();
                     app.addTask(nume);
                     System.out.println("[ADDED] " + nume);
                 }
                 case 2 -> {
                     System.out.println("[DELETE] Specifica nume: ");
-                    String nume = sc.next();
+                    String nume = sc.nextLine();
                     app.deleteTask(nume);
                     System.out.println("[DELETED] " + nume);
                 }
                 case 3 -> {
                     System.out.println("[COMPLETE] Specifica nume: ");
-                    String nume = sc.next();
+                    String nume = sc.nextLine();
                     app.completeTask(nume);
                     System.out.println("[COMPLETED] " + nume);
                 }
@@ -53,6 +55,7 @@ public class TaskManagerApp {
                 }
                 case 5 -> {
                     running = false;
+                    app.saveTaskToFile("tasks.txt");
                 }
                 default -> {
                     System.out.println("Optiune invalida");
@@ -98,4 +101,40 @@ public class TaskManagerApp {
         System.out.println("Nu exista taskul {" + name + "}");
         return null;
     }
+    public void saveTaskToFile(String filename) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (Task task : tasks) {
+                bw.write(task.getId() + ";" + task.getName() + ";" + task.isCompleted() + "\n");
+                bw.newLine();
+            }
+            System.out.println("[SAVED] " + filename);
+        } catch (IOException e) {
+            System.out.println("[ERROR] Eroare la scrierea in fisier: " + e.getMessage());
+        }
+    }
+    public void loadTaskFromFile(String filename) {
+        tasks.clear(); // ne asiguram ca nu avem nimic in tasks inainte sa citim
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(";");
+                if (data.length == 3) {
+                    String name = data[1];
+                    boolean isCompleted = Boolean.parseBoolean(data[2]);
+                    Task task = new Task(name);
+                    if (isCompleted) {
+                        task.setCompleted();
+                    }
+                    tasks.add(task);
+                }
+
+            }
+            System.out.println("[LOADED] " + filename);
+        } catch (FileNotFoundException e) {
+            System.out.println("[ERROR] Fisierul nu exista. Se va crea la inchiderea programului.");
+        } catch (IOException e) {
+            System.out.println("[ERROR] Eroare la citirea din fisier: " + e.getMessage());
+        }
+    }
+
 }
